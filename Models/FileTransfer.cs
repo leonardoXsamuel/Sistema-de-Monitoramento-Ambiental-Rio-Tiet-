f1f6993c575a63ee6197093585e7de3f5c5c4a89
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-15 10:56:43 -0300

    feat: Inserindo algumas anotações de validação nos atributos da entidade de FileTransfer e User.

--- Models/FileTransfer.cs
+++ Models/FileTransfer.cs
@@ -4,7 +4,7 @@ public class FileTransfer
 {
     public int Id { get; set; }
     public string NomeOriginal { get; set; } = string.Empty;  // nome original
-    public string StoredName { get; set; } = string.Empty;  // nome no disco (GUID) -- pesqiosar pq preciso do nome no disco
+    public string NomeGeradoCript { get; set; } = string.Empty;  // nome q vai ser criado no FileService 
     public string TipoConteudo { get; set; } = string.Empty;  // ex: application/pdf
     public long TamanhoBytes { get; set; }
     public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,23 @@
 ﻿
+using ApsMartChat.Models.Enum;
+using System.ComponentModel.DataAnnotations;
+
 namespace ApsMartChat.Models;
 
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public string DisplayName { get; set; } = string.Empty;
-    public string Role { get; set; } = "inspetor"; // inspector | coordenador
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    [Required]
+    [StringLength(155, MinimumLength = 3)]
+    public string Username { get; set; } = null!;
 
+    [Required]
+    [StringLength(155, MinimumLength = 3)]
+    public string DisplayName { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+    public UserRole Role { get; set; } = UserRole.Inspetor; // inspetor | coordenador
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public ICollection<Message> Messages { get; set; } = new List<Message>();
     public ICollection<FileTransfer> FileTransfers { get; set; } = new List<FileTransfer>();
 }
