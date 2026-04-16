84897ca09fb8ae1212bbc9a3fd11b778de956335
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-16 19:55:43 -0300

    refactor: ajustando nomes e parametros dos metodos da interface

--- Services/File/IFileService.cs
+++ Services/File/IFileService.cs
@@ -1,10 +1,11 @@
 ﻿using ApsMartChat.DTOs;
+using ApsMartChat.DTOs.FileTransfer;
 
 namespace ApsMartChat.Services.File;
 
 public interface IFileService
 {
-    Task<FileTransferDto> UploadAsync(IFormFile file, string username, int roomId, string baseUrl);
-    Task<(Stream stream, string contentType, string fileName)?> DownloadAsync(int fileId);
-    Task<List<FileTransferDto>> GetByRoomAsync(int roomId, string baseUrl);
+    Task<FileTransferResponseDTO> UploadDeArquivoAsync(IFormFile file, string username, int roomId, string baseUrl);
+    Task<(Stream stream, string contentType, string fileName)?> DownloadDeArquivoAsync(int fileId);
+    Task<List<FileTransferResponseDTO>> GetFilesByRoomAsync(int roomId);
 }
