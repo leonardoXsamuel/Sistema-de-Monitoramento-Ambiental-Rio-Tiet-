3bd9fd09602a09ab38f04262629304e4083d8a12
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-16 20:05:19 -0300

    refactor: removendo atribuição da entidade e deixando no service para evitar inconstancia nesse valor.

--- Models/Message.cs
+++ Models/Message.cs
@@ -4,7 +4,7 @@ public class Message
 {
     public int Id { get; set; }
     public string Content { get; set; } = string.Empty;
-    public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    public DateTime SentAt { get; set; }
 
     public int SenderId { get; set; }
     public User Sender { get; set; } = null!;
