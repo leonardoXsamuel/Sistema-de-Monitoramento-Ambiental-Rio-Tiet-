a2677e2d8ddaf7177824560d026d9e3719d0b754
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-16 20:20:44 -0300

    refactor: Ajustando retornos e nomes dos métodos.

--- Services/Message/MessageService.cs
+++ Services/Message/MessageService.cs
@@ -1,48 +1,39 @@
 using ApsMartChat.DTOs;
+using ApsMartChat.DTOs.Message;
+using ApsMartChat.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using EnviroChat.API.Data;
-using EnviroChat.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ApsMartChat.Services.Message;
 
-public interface IMessageService
-{
-    Task<MessageDto> SaveAsync(string content, string username, int roomId);
-    Task<List<MessageDto>> GetHistoryAsync(int roomId, int page = 1, int pageSize = 50);
-}
-
 public class MessageService : IMessageService
 {
     private readonly AppDbContext _db;
+    private readonly IMapper _mapper;
 
-    public MessageService(AppDbContext db) => _db = db;
+    public MessageService(AppDbContext db, IMapper mapper) => (_db, _mapper) = (db, mapper);
 
-    public async Task<MessageDto> SaveAsync(string content, string username, int roomId)
+    public async Task<MessageResponseDTO> SaveMessageAsync(string content, string username, int roomId)
     {
-        var user = await _db.Users.FirstAsync(u => u.Username == username);
+        var user = await _db.Users.FirstOrDefaultAsync(u => u.Username == username) ?? throw new Exception();
 
-        var message = new Message
+        var message = new Models.Message
         {
-            Content  = content,
+            Content = content,
             SenderId = user.Id,
-            RoomId   = roomId,
-            SentAt   = DateTime.UtcNow
+            RoomId = roomId,
+            SentAt = DateTime.UtcNow
         };
 
         _db.Messages.Add(message);
         await _db.SaveChangesAsync();
 
-        return new MessageDto(
-            message.Id,
-            message.Content,
-            user.Username,
-            user.DisplayName,
-            message.SentAt,
-            roomId
-        );
+        return _mapper.Map<MessageResponseDTO>(message);
     }
 
-    public async Task<List<MessageDto>> GetHistoryAsync(int roomId, int page = 1, int pageSize = 50)
+    public async Task<List<MessageResponseDTO>> GetHistoryOfMessagesAsync(int roomId, int page = 1, int pageSize = 50)
     {
         return await _db.Messages
             .Include(m => m.Sender)
@@ -50,14 +41,7 @@ public class MessageService : IMessageService
             .OrderByDescending(m => m.SentAt)
             .Skip((page - 1) * pageSize)
             .Take(pageSize)
-            .Select(m => new MessageDto(
-                m.Id,
-                m.Content,
-                m.Sender.Username,
-                m.Sender.DisplayName,
-                m.SentAt,
-                m.RoomId
-            ))
+            .ProjectTo<MessageResponseDTO>(_mapper.ConfigurationProvider)
             .ToListAsync();
     }
 }
