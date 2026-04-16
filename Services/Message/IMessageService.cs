735e505c3d9fcba5893005dd3485e62c28cd2367
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-15 21:06:03 -0300

    feat: Criando MessageService-base.

---
+++ Services/Message/IMessageService.cs
@@ -0,0 +1,9 @@
+﻿using ApsMartChat.DTOs.Message;
+
+namespace ApsMartChat.Services.Message;
+
+public interface IMessageService
+{
+    Task<MessageResponseDTO> SaveAsync(string content, string username, int roomId);
+    Task<List<MessageResponseDTO>> GetHistoryAsync(int roomId, int page = 1, int pageSize = 50);
+}
