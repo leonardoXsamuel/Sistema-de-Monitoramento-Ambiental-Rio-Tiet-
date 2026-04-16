abfec9f0789f7fb66425eff6bfa474e56c2fcf15
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-16 19:54:04 -0300

    refat: ajustando using do appContext.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,3 +1,4 @@
+using ApsMartChat.Models;
 using EnviroChat.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,10 +8,10 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<User>         Users         { get; set; }
-    public DbSet<Message>      Messages      { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Message> Messages { get; set; }
     public DbSet<FileTransfer> FileTransfers { get; set; }
-    public DbSet<ChatRoom>     ChatRooms     { get; set; }
+    public DbSet<ChatRoom> ChatRooms { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
