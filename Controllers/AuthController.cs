26b78d17dca240adad0ed39dae06ee2279d25167
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-17 20:57:28 -0300

    style: ajustando espaçamento dos metodos.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using ApsMartChat.DTOs;
+using ApsMartChat.DTOs.Auth;
 using ApsMartChat.Services.Auth;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,24 +8,26 @@ using Microsoft.AspNetCore.Mvc;
 public class AuthController : ControllerBase
 {
     private readonly IAuthService _auth;
-
+    
     public AuthController(IAuthService auth) => _auth = auth;
 
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterRequest req)
     {
-        var result = await _auth.RegisterAsync(req);
+        var result = await _auth.RegistrarUsuarioAsync(req);
         if (result is null)
             return Conflict(new { message = "Nome de usuário já existe." });
+
         return Ok(result);
     }
 
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest req)
     {
-        var result = await _auth.LoginAsync(req);
+        var result = await _auth.LoginDeUsuarioAsync(req);
         if (result is null)
             return Unauthorized(new { message = "Credenciais inválidas." });
+
         return Ok(result);
     }
 }
