c61fd54a30e701b9d25b64f2ea4bf8df34bcca50
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-19 00:47:15 -0300

    feat: gerando controller de ChatRoom

---
+++ Controllers/ChatRoomController.cs
@@ -0,0 +1,26 @@
+﻿using ApsMartChat.DTOs.ChatRoom;
+using ApsMartChat.Services.File;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApsMartChat.Controllers;
+
+public class ChatRoomController : ControllerBase
+{
+    private readonly IChatRoomService _service;
+
+    public ChatRoomController(IChatRoomService service) => _service = service;
+
+    [HttpPut("updateChatRoom/{roomId}")]
+    public async Task<ActionResult<ChatRoomResponseDTO>> AlterarNomeChatRoom([FromRoute] int roomId, [FromBody] ChatRoomUpdateDTO chatRoomUpdateDTO)
+    {
+        var dto = await _service.AlterarNomeChatRoom(roomId, chatRoomUpdateDTO);
+
+        if (dto is null)
+            return BadRequest();
+
+        return Ok(dto);
+    }
+
+
+
+}
