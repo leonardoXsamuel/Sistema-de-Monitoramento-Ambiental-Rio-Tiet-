bd1395dba40bf57d1b69d624cbadda6fd712ebe6
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-04-22 21:10:53 -0300

    feat: adicionando exceção customizada para tipos de arquivos inválidos.

---
+++ Exceptions/InvalidTypeFileException.cs
@@ -0,0 +1,7 @@
+﻿namespace ApsMartChat.Exceptions;
+
+public class InvalidTypeFileException : Exception
+{
+    public InvalidTypeFileException(string msg) : base(msg) { }
+    public InvalidTypeFileException() : base("O tipo do arquivo é inválido.") { }
+}
